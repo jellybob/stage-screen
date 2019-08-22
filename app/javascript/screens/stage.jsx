65af6bf84c4ed801f2922a57bd3d5a81d2e184fd
Jon Wood <jon@blankpad.net>
2019-08-22 13:41:52 +0200

    Super shonky stage view

--- app/javascript/screens/stage.jsx
+++ app/javascript/screens/stage.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+class TopBar extends React.Component {
+  render() { return <div className="top-bar" /> }
+}
+
+class MessagesBar extends React.Component {
+  render() { return <div className="bottom-bar" /> }
+}
+
+class ContentContainer extends React.Component {
+  render() { return <div className="content" /> }
+}
+
 class StageScreen extends React.Component {
   render() {
-    return <h1>Stage</h1>;
+    return (
+      <div>
+        <TopBar />
+        <ContentContainer />
+        <MessagesBar />
+      </div>
+    )
   }
 }
 
