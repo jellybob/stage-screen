14727437728ade87be39835c3532acd3b698f96d
Jon Wood <jon@blankpad.net>
2019-08-23 14:05:14 +0200

    Support for YouTube videos

--- app/javascript/screens/content_container.jsx
+++ app/javascript/screens/content_container.jsx
@@ -1,4 +1,53 @@
 import React from 'react';
+import YouTube from 'react-youtube';
+
+class ImageContainer extends React.Component {
+  componentDidMount() {
+    this.completionTimer = setTimeout(this.props.onComplete, 2500);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.completionTimer);
+  }
+
+  render() {
+    return <img src={this.props.content.url} id="content-image" />
+  }
+}
+
+class YouTubeContainer extends React.Component {
+  render() {
+    var videoUrl = this.props.content.url.split('/');
+    var videoId = videoUrl[videoUrl.length - 1];
+    var opts = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      playerVars: {
+        autoplay: 1,
+        controls: 0,
+        disablekb: 1, // Disable YouTube keyboard shortcuts
+        modestbranding: 1, // Remove YouTube branding
+        iv_load_policy: 3, // Don't show annotations
+        fs: 0, // Disable the fullscreen button
+      },
+    }
+
+    return (
+      <YouTube
+        videoId={videoId}
+        id="content-youtube"
+        opts={opts}
+        onEnd={this.props.onComplete}
+        onError={this.props.onComplete}
+        />
+    )
+  }
+}
+
+var contentComponents = {
+  'image': ImageContainer,
+  'youtube': YouTubeContainer,
+};
 
 class ContentContainer extends React.Component {
   constructor(props) {
@@ -10,7 +59,7 @@ class ContentContainer extends React.Component {
 
   componentDidMount() {
     this.nextItem();
-    this.timeout = setInterval(this.nextItem.bind(this), 10000);
+    //this.timeout = setInterval(this.nextItem.bind(this), 10000);
   }
 
   componentWillUnmount() {
@@ -31,7 +80,9 @@ class ContentContainer extends React.Component {
       return null;
     }
 
-    return <img src={content.url} id="content-image" />
+    console.log(content.content_type);
+    var ContentComponent = contentComponents[content.content_type];
+    return <ContentComponent content={content} onComplete={this.nextItem.bind(this)} />;
   }
 
   render() {
