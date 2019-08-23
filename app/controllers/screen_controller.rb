aafa169b57bf281de0ec5ef2a4ae28e7c3f65347
Jon Wood <jon@blankpad.net>
2019-08-23 15:19:28 +0200

    Ensure a display is provisioned

--- app/controllers/screen_controller.rb
+++ app/controllers/screen_controller.rb
@@ -3,5 +3,19 @@
 class ScreenController < ApplicationController
   layout "screen"
 
-  def show; end
+  def show
+    find_or_create_display
+  end
+
+  private
+
+  def find_or_create_display
+    if cookies.signed[:display_id].nil?
+      display = Display.create!
+      cookies.signed[:display_id] = display.display_id
+      return display
+    end
+
+    Display.where(display_id: cookies.signed[:display_id]).first
+  end
 end
